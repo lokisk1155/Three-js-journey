be8342ef396abfc9d40c304f92314ff7957d3797
lokisk1155 <shawn12345d@gmail.com>
2023-05-15 15:19:20 -0400

    basic scene

--- chapter1/BasicScene/script.js
+++ chapter1/BasicScene/script.js
@@ -17,7 +17,9 @@ const sizes = {
 
 // Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3
 scene.add(camera);
+
 
 // Canvas
 const canvas = document.querySelector("canvas.webgl");
